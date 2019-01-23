7b05e3668e79b7785e2c30760bbffea0cedd662c
skobraf <skobraf@gmail.com>
2019-01-23 14:43:18 +0000

    added properties to storePicker

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+	myInput = React.createRef();
+
+	goToStore = (event) => {
+		event.preventDefault();
+		
+	}
 	render() {
 		return (
-			<form className="store-selector">
+			<form className="store-selector" onSubmit={this.goToStore}>
 				<h1>Please Enter A Store</h1>
-				<input type="text" required placeholder="Store Name"/>
+				<input 
+					type="text" 
+					ref={this.myInput}
+					required 
+					placeholder="Store Name"
+					defaultValue={getFunName()}
+					/>
 				<button type="submit">Visit Store</button>
 			</form>
 			);
