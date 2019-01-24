374775546e7d76e45fa1907f35bf5c823591162d
skobraf <skobraf@gmail.com>
2019-01-24 17:08:53 +0000

    added price form

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import AddFishForm from './AddFishForm';
 
 class Inventory extends React.Component {
 	render() {
 		return (
-			<div className="inventory">Inventroy</div>
+			<div className="inventory">
+				<AddFishForm />
+			</div>
 			);
 	}
 }
