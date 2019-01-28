5e7fac8fc142a0a044a6a3ea3313d47a52e4a426
skobraf <skobraf@gmail.com>
2019-01-28 16:17:13 +0000

    updated store picker

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -4,10 +4,12 @@ import { getFunName } from '../helpers';
 class StorePicker extends React.Component {
 	myInput = React.createRef();
 
-	goToStore = (event) => {
-		event.preventDefault();
-		const storeName = this.myInput.value.value;
-		// change page to /store/the-input
+	goToStore = event => {
+	    // 1. Stop the form from submitting
+	    event.preventDefault();
+	    // 2. get the text from that input
+	    const storeName = this.myInput.current.value;
+	    // 3. Change the page to /store/whatever-they-entered
 		this.props.history.push(`/store/${storeName}`);
 	}
 	render() {
