8771eededd3e95aa619c898d9cbb368d558ed9f3
skobraf <skobraf@gmail.com>
2019-01-30 20:29:35 +0000

    added the file for production

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.83e1490c9bc737ee35cd5729108b9b24.js"
+  "/precache-manifest.2871bda8a644c1d113df2c5fa824bddd.js"
 );
 
 workbox.clientsClaim();
